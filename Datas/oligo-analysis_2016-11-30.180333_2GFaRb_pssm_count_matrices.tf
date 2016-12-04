acb0276b16dd655c2614bf8782212c8121f513fe
mcardon-upmc <melissa.cardon@etu.upmc.fr>
2016-12-04 13:15:58 +0100

    Ajout des donnees PSSM (question1)

---
+++ Datas/oligo-analysis_2016-11-30.180333_2GFaRb_pssm_count_matrices.tf
@@ -0,0 +1,152 @@
+AC  assembly_1
+XX
+ID  assembly_1
+XX
+DE  dmwCTGTCATmwa
+P0       A     C     G     T
+1        7     1     5     7
+2       10     7     1     2
+3        7     2     1    10
+4        0    19     0     1
+5        0     1     0    19
+6        0     0    20     0
+7        0     2     2    16
+8        3    17     0     0
+9       19     0     1     0
+10       0     2     0    18
+11       7    10     0     3
+12       7     2     4     7
+13       9     4     3     4
+XX
+CC  program: feature
+CC  matrix.nb: 1
+CC  matrix.nb: 1
+CC  sites: 20
+CC  consensus.strict: aatCTGTCATcaa
+CC  consensus.strict.rc: TTGATGACAGATT
+CC  consensus.IUPAC: dmwCTGTCATmwa
+CC  consensus.IUPAC.rc: TWKATGACAGWKH
+CC  consensus.regexp: [agt][ac][at]CTGTCAT[ac][at]a
+CC  consensus.regexp.rc: T[AT][GT]ATGACAG[AT][GT][ACT]
+XX
+//
+AC  assembly_2
+XX
+ID  assembly_2
+XX
+DE  kyATCCGGCmt
+P0       A     C     G     T
+1        3     2    22    16
+2        4    17     9    13
+3       43     0     0     0
+4        0     2     0    41
+5        0    38     5     0
+6        9    34     0     0
+7        1     1    39     2
+8        6     2    35     0
+9        0    42     1     0
+10      16    14     9     4
+11      10     6     2    25
+XX
+CC  program: feature
+CC  matrix.nb: 2
+CC  matrix.nb: 2
+CC  sites: 43
+CC  consensus.strict: gcATCCGGCat
+CC  consensus.strict.rc: ATGCCGGATGC
+CC  consensus.IUPAC: kyATCCGGCmt
+CC  consensus.IUPAC.rc: AKGCCGGATRM
+CC  consensus.regexp: [gt][ct]ATCCGGC[ac]t
+CC  consensus.regexp.rc: A[GT]GCCGGAT[AG][AC]
+XX
+//
+AC  assembly_3
+XX
+ID  assembly_3
+XX
+DE  wmTCTGTCATmwa
+P0       A     C     G     T
+1        5     1     2     6
+2        6     6     0     2
+3        3     0     1    10
+4        0    14     0     0
+5        0     0     0    14
+6        0     0    14     0
+7        0     2     0    12
+8        3    11     0     0
+9       14     0     0     0
+10       0     1     0    13
+11       7     4     0     3
+12       6     2     2     4
+13       9     2     1     2
+XX
+CC  program: feature
+CC  matrix.nb: 3
+CC  matrix.nb: 3
+CC  sites: 14
+CC  consensus.strict: taTCTGTCATaaa
+CC  consensus.strict.rc: TTTATGACAGATA
+CC  consensus.IUPAC: wmTCTGTCATmwa
+CC  consensus.IUPAC.rc: TWKATGACAGAKW
+CC  consensus.regexp: [at][ac]TCTGTCAT[ac][at]a
+CC  consensus.regexp.rc: T[AT][GT]ATGACAGA[GT][AT]
+XX
+//
+AC  assembly_4
+XX
+ID  assembly_4
+XX
+DE  mGGCCTACar
+P0       A     C     G     T
+1        3     6     0     0
+2        1     0     7     1
+3        0     0     9     0
+4        0     9     0     0
+5        0     9     0     0
+6        0     0     0     9
+7        9     0     0     0
+8        0     9     0     0
+9        5     1     2     1
+10       4     1     4     0
+XX
+CC  program: feature
+CC  matrix.nb: 4
+CC  matrix.nb: 4
+CC  sites: 9
+CC  consensus.strict: cGGCCTACaa
+CC  consensus.strict.rc: TTGTAGGCCG
+CC  consensus.IUPAC: mGGCCTACar
+CC  consensus.IUPAC.rc: YTGTAGGCCK
+CC  consensus.regexp: [ac]GGCCTACa[ag]
+CC  consensus.regexp.rc: [CT]TGTAGGCC[GT]
+XX
+//
+AC  assembly_5
+XX
+ID  assembly_5
+XX
+DE  nsGCTTATch
+P0       A     C     G     T
+1        2     2     2     2
+2        0     5     2     1
+3        0     0     8     0
+4        0     8     0     0
+5        0     0     0     8
+6        0     0     0     8
+7        8     0     0     0
+8        0     0     0     8
+9        1     5     1     1
+10       2     4     0     2
+XX
+CC  program: feature
+CC  matrix.nb: 5
+CC  matrix.nb: 5
+CC  sites: 8
+CC  consensus.strict: -cGCTTATcc
+CC  consensus.strict.rc: GGATAAGCG-
+CC  consensus.IUPAC: nsGCTTATch
+CC  consensus.IUPAC.rc: DGATAAGCSN
+CC  consensus.regexp: [acgt][cg]GCTTATc[act]
+CC  consensus.regexp.rc: [AGT]GATAAGC[CG][ACGT]
+XX
+//

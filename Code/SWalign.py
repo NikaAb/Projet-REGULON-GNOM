1c9ef8d5781fb475194b4d37753b4e56d1e5126c
NikaAb <nikaabdollahi@gmail.com>
2016-12-08 10:09:21 +0100

    Ajout de matrice des scores

--- Code/SWalign.py
+++ Code/SWalign.py
@@ -1,27 +1,31 @@
 import sys
 import numpy as np
+from f_import2 import *
 
 ########################################################################
 
-def alignit(s1,s2,gap,subs):
+def alignit(PSSM1,PSSM2,gap,Metric):
 	# matrice des distances
-	m = range(len(s2)+1)
+	m = range(len(PSSM2[0])+1)
+	#print m
 	# matrice des chemins
-	for i in range(len(s2)+1):
-		m[i] = range(len(s1)+1)
+	for i in range(len(PSSM2[0])+1):
+		m[i] = range(len(PSSM1[0])+1)
+	#print m
 	# root cell
 	m[0][0] = (0, 'o')
 	# first line
-	for j in range(1,len(s1)+1):
+	for j in range(1,len(PSSM1[0])+1):
 		#v=m[0][j-1][0] + gap
 		m[0][j]=(0, 'g')
 	# first column
-	for i in range(1,len(s2)+1):
-		m[i][0]=(0, 'h')	
+	for i in range(1,len(PSSM2[0])+1):
+		m[i][0]=(0, 'h')
 	# tab
-	for i in range(1,len(s2)+1):
-		for j in range(1,len(s1)+1):
-			distd = substitue(s2[i-1], s1[j-1],subs) + m[i-1][j-1][0]
+
+	for i in range(1,len(PSSM2[0])+1):
+		for j in range(1,len(PSSM1[0])+1):
+			distd = ChooseMetric(PSSM2[:,i-1], PSSM1[:,j-1],Metric) + m[i-1][j-1][0]
 			disth = gap + m[i-1][j][0]
 			distg = gap + m[i][j-1][0]
 			if distd >= disth and distd >= distg:
@@ -42,7 +46,6 @@ def substitue(res1, res2,subs):
 
 ########################################################################
 
-
 def findIndice(res):
 	if res=="A":
 		Indice=0
@@ -55,15 +58,17 @@ def findIndice(res):
 	return Indice
 ########################################################################
 #Retrouver le chemin
-def backtrack(m):
+def backtrack(m, imax,jmax):
 	#print "distance = ",m[len(s1)][len(s2)]
 	#print m
 	#print c
 	# backtrack
 	#nb col
-	j = len(m[0])-1
+	j = jmax
+	#len(m[0])-1
 	#nb lignes
-	i = len(m)-1
+	i =imax
+	#len(m)-1
 	chemin = ''
 	while (i != 0 or j != 0):
 		if i < 0 or j < 0:
@@ -101,7 +106,7 @@ def backtrack(m):
 ## Print aligment
 def printali(s1,s2,sa):
 	ii = 0
-	print sa
+	#print sa
 	for i in range(len(sa)):
 		if sa[i] == 'd' or sa[i] == 'g':
 			sys.stdout.write(s1[ii])
@@ -117,21 +122,52 @@ def printali(s1,s2,sa):
 		else:
 			sys.stdout.write('-')
 	print ''
+########################################################################
+def solve(mat):
+    print " ", " ".join([str(x) for x in xrange(len(mat))])
+    for i, x in enumerate(mat):
+        print i, " ".join([str(y) for y in x])
 
 
-
+########################################################################
+def FindIndiceMax(matrix):
+	Nb_line=len(matrix)
+	Nb_colomn=len(matrix[0])
+	Valmax=0
+	imax=0
+	jmax=0
+	for i in range(Nb_line) :
+		for j in range(Nb_colomn):
+			#print matrix[i][j][0] 
+			if matrix[i][j][0] >Valmax:
+				Valmax=matrix[i][j][0]
+		 		imax=i
+		 		jmax=j
+	return Valmax,imax,jmax
+########################################################################
+def Score_Calculator(PSSM1,PSSM2,gap,Metric):
+	m= alignit(PSSM1,PSSM2,gap,Metric)
+	v,imax,jmax=FindIndiceMax(m)
+	sa=backtrack(m,imax,jmax)
+	return v
+########################################################################
+def Matix_Score(listPSSM):
+    n=len(listPSSM)
+    Matix_Score=np.zeros((n,n))
+    List_Of_PSSM_Ind=listCombinaton(list(range(0, len(PSSM_all_freqs))))
+    for Deux_PSSM in List_Of_PSSM_Ind:
+        A=listPSSM[Deux_PSSM[0]]
+        B=listPSSM[Deux_PSSM[1]]
+        if len(A)>len(B):
+            score= Score_Calculator(A,B,-1,"SSD")
+        else :
+            score= Score_Calculator(B,A,-1,"SSD")
+        #print Deux_PSSM,score
+        Matix_Score[(Deux_PSSM[0]),(Deux_PSSM[1])]=score
+        Matix_Score[(Deux_PSSM[1]),(Deux_PSSM[0])]=score
+    return  Matix_Score
 ########################################################################
 #								Main
 ########################################################################
 
-subs=np.matrix([[3,0,1,0],[0,3,0,1],[1,0,3,0],[0,1,0,3]])
-
-s1="AGCACACA"
-s2="ACACACTA"
-
-
-m=alignit(s1,s2,-1,subs)
-print type(m)
-sa=backtrack(m)
-printali(s1,s2,sa)
-printMatAli(matAli, s1, s2)
+print Matix_Score(PSSM_all_freqs)
